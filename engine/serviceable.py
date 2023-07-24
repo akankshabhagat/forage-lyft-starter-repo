1a17da85cdc61cbcba97c884278abe33850eb641
akanksha <akankshabhagat606@gmail.com>
2023-07-24 19:35:02 +0530

    changes according to uml diagram
    not perfect but what i understood is reflected here

---
+++ engine/serviceable.py
@@ -0,0 +1,5 @@
+from abc import ABC
+class servicable(ABC):
+    def needs_service(self):
+        pass
+    
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -2,12 +2,12 @@ from abc import ABC
 
 from car import Car
 
-
+from serviceable import servicable
 class WilloughbyEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+        super().__init__(last_service_date,servicable)
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
         return self.current_mileage - self.last_service_mileage > 60000
